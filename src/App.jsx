a2dcf0a87aac487b1aa996d032a9cf8082ce4d66
Vedant Goradiya <Vedantgoradiya1998@gmail.com>
2026-05-19 17:57:07 -0400

    Added jobBoard page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import JobBoard from "./Pages/Job-Board/JobBoard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/jobBoard" element={<JobBoard />} />
       </Routes>
     </div>
   );
